42c2fa202f8d89c3a0cd319d997d113b81fc8ba8
Lukas Fülling <lukas@k40s.net>
2020-06-08 01:02:23 +0200

    get it to work

--- main.go
+++ main.go
@@ -8,54 +8,67 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"strconv"
 	"time"
 )
 
 type Config struct {
-	Domains []string
-	Port    int
+	Domains       []string
+	ListenAddress string
 }
 
 var config Config
 
-func loadCertificates() ([]*x509.Certificate, error) {
+func loadCertificates() ([]*x509.Certificate, string) {
+	startTime := time.Now().UnixNano()
 	var certs []*x509.Certificate
+	var fetchMetrics = ""
 
 	for _, d := range config.Domains {
 		log.Println("Loading certificate for '" + d + "'...")
 		resp, err := http.Get("https://" + d + "/")
 		if err != nil {
-			log.Fatalln("Error fetching '"+d+"'!", err)
+			log.Println("Error fetching '"+d+"'!", err)
+			fetchMetrics += "cert_fetch_success{domain=\"" + d + "\"} 0\n"
 		} else {
 			if resp.TLS != nil {
 				certificates := resp.TLS.PeerCertificates
 				if len(certificates) > 0 {
 					certs = append(certs, certificates[0])
+					fetchMetrics += "cert_fetch_success{domain=\"" + d + "\"} 1\n"
 				} else {
-					log.Fatalln("No certificates given for '" + d + "'!")
+					log.Println("No certificates given for '" + d + "'!")
+					fetchMetrics += "cert_fetch_success{domain=\"" + d + "\"} 0\n"
 				}
 			} else {
-				log.Fatalln("TLS properties nil for '"+d+"'!", err)
+				log.Println("TLS properties nil for '"+d+"'!", err)
+				fetchMetrics += "cert_fetch_success{domain=\"" + d + "\"} 0\n"
 			}
 
 		}
+		fetchMetrics += "cert_fetch_duration{domain=\"" + d + "\"} " + strconv.FormatInt(time.Now().UnixNano()-startTime, 10) + "\n"
 	}
-	return certs, nil
+	return certs, fetchMetrics
 }
 
 func renderMetricsResponse() (string, error) {
-	certs, err := loadCertificates()
-	if err != nil {
-		return "", err
-	}
+	certs, metrics := loadCertificates()
 
-	res := "# HELP cert_not_after The primary certificate's NotAfter date as unix time'.\n" +
-		"# TYPE cert_not_after gauge"
+	res := "# HELP cert_not_before The primary certificates NotBefore date as unix time'.\n" +
+		"# TYPE cert_not_before gauge\n" +
+		"# HELP cert_not_after The primary certificates NotAfter date as unix time'.\n" +
+		"# TYPE cert_not_after gauge\n" +
+		"# HELP cert_fetch_duration Duration of the http call in nanoseconds.\n" +
+		"# TYPE cert_fetch_duration gauge\n" +
+		"# HELP cert_fetch_success Success of the http call as a 0/1 boolean.\n" +
+		"# TYPE cert_fetch_success gauge\n" + metrics
 	for _, crt := range certs {
 		for _, dnsName := range crt.DNSNames {
-			res += `cert_not_after{domain="` + dnsName + `"} ` + string(crt.NotAfter.Unix())
+			res += `cert_not_before{domain="` + dnsName + `"} ` + strconv.FormatInt(crt.NotBefore.Unix(), 10) + "\n"
+			res += `cert_not_after{domain="` + dnsName + `"} ` + strconv.FormatInt(crt.NotAfter.Unix(), 10) + "\n"
 		}
 	}
+
 	return res, nil
 }
 
@@ -92,11 +105,14 @@ func main() {
 	}
 
 	server := &http.Server{
-		Addr:         "localhost:" + string(config.Port),
+		Addr:         config.ListenAddress,
 		Handler:      http.HandlerFunc(handleMetrics),
 		ReadTimeout:  10 * time.Second,
 		WriteTimeout: 10 * time.Second,
 	}
 	log.Println("Starting server...")
-	log.Fatalln("Unable to start server!", server.ListenAndServe())
+	err2 := server.ListenAndServe()
+	if err2 != nil {
+		log.Fatalln("Unable to start server!", err2)
+	}
 }
